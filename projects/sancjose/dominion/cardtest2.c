6132dab81da8eb452a15e88d93ce94c3425bd12d
Jose L Sanchez <jl_sanche@mac.com>
2018-10-25 20:15:47 -0700

    smithy unit test complete

---
+++ projects/sancjose/dominion/cardtest2.c
@@ -0,0 +1,59 @@
+
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "adventurer"
+
+int main() {
+    int newCards = 0;
+    int discarded = 1;
+    int xtraCoins = 0;
+    int shuffledCards = 0;
+
+    int i, j, m;
+    int handpos = 0, bonus = 0;
+    int remove1, remove2;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, gardens, smithy, outpost};
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+
+	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
+
+	// ----------- TEST 1: choice1 = 1 = +2 cards --------------
+	printf("TEST 1: choice1 = 1 = +2 cards\n");
+
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+	
+	cardEffect(steward, 0,0,0, &testG, handpos, &bonus);
+
+	newCards = 2;
+	xtraCoins = 0;
+	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
+	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
+	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
+	assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
+	assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
+	assert(testG.coins == G.coins + xtraCoins);
+
+	
+
+
+	printf("\n >>>>> SUCCESS: Testing complete %s card <<<<<\n\n", TESTCARD);
+
+
+	return 0;
+}
