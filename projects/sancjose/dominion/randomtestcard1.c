13d45d6a21320ad4f7ba09ea3c9e750cb5c55fba
Jose L Sanchez <jl_sanche@mac.com>
2018-11-12 10:25:47 -0800

    created randomtestcards

---
+++ projects/sancjose/dominion/randomtestcard1.c
@@ -0,0 +1,13 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
